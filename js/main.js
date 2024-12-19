42aeb82036f8472be7adb80badabaf3570d01d76
abcd-EGH <pyu37856@gmail.com>
2024-12-19 10:04:56 +0900

    [STYLE] modify from title to about section

--- js/main.js
+++ js/main.js
@@ -312,9 +312,9 @@ TxtRotate.prototype.tick = function() {
   this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
   var that = this;
-  var delta = 300 - Math.random() * 100;
+  var delta = 150 - Math.random() * 100;
 
-  if (this.isDeleting) { delta /= 2; }
+  if (this.isDeleting) { delta /= 2.0; }
 
   if (!this.isDeleting && this.txt === fullTxt) {
     delta = this.period;
